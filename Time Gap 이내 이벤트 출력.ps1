c15e6eddc47f18af9973c62321e9efffc02701b5
unknown <empty7792@gmail.com>
2019-10-23 17:40:54 +0900

    save to Csv

---
+++ Time Gap 이내 이벤트 출력.ps1
@@ -0,0 +1,24 @@
+﻿$Logfiles= Get-WinEvent -ListLog *
+
+$Current = Get-Date
+$Before = (Get-Date) - (New-TimeSpan -Second 30)
+
+'Time Gap'
+$Current
+$Before
+
+foreach($Logfile in $Logfiles){
+    if($Logfile.RecordCount -gt 0){
+        
+        $Result = Get-WinEvent -LogName $Logfile.LogName | Where {$_.TimeCreated -ge $Before -And $_.TimeCreated -le $Current} |
+                  Select -Propert 'TimeCreated', 'Id'
+        $Logname = $Logfile.LogName
+        Write-Host -NoNewline "Current : " 
+        $Logname
+        if($Result){
+            $Logname = $Logname -replace "/", "%4"
+            Write-Host -NoNewline 'Filename : ' $Logname
+            $Result | Export-Csv -Path "C:\Users\dfrc\Desktop\Logfile Dir\$Logname.csv"
+        }    
+    }
+}
